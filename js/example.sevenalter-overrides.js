48322df33a8d5e7487427a890e98b51a02715dcd
Frank Yonnetti <frank@designhammer.com>
2022-01-11 14:50:20 -0500

    Added JS overrides file

--- js/example.sevenalter-overrides.js
+++ js/example.sevenalter-overrides.js
@@ -1,5 +1,5 @@
 /**
- * Duplicate this file and rename it sevenalter-overrides.css
+ * Duplicate this file and rename it sevenalter-overrides.js
  *
  * This file can be used to override admin theme styles for all unique cases.
  * It can remain empty until needed. ⚠️ Failing to create this file will cause a console error.
